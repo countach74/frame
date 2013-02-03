f94c73356a6b12a6a819cbb48d3954dcd02ac559
Tim Radke <countach74@gmail.com>
2013-02-03 10:23:23 -0800

    Converted HTTP server to be asynchronous. Still needs a lot of work.

--- frame/server/http.py
+++ frame/server/http.py
@@ -11,16 +11,40 @@ import traceback
 import cgi
 from jinja2 import Environment, PackageLoader, ChoiceLoader, FileSystemLoader
 
+# Needed for asynchronous server
+import select
 
-class Connection(threading.Thread):
+
+class Connection(object):
 	def __init__(self, server, accept):
 		self.server = server
 		self.socket, self.addr = accept
-		threading.Thread.__init__(self)
 
-	def run(self):
+		self.read_buffer = []
+		self.write_buffer = []
+
+	def fileno(self):
+		return self.socket.fileno()
+
+	def handle_connect(self):
+		pass
+
+	def handle_read(self):
 		request = self.socket.recv(self.server.max_read)
+		self.handle_request(request)
 
+	def handle_write(self):
+		data = self.write_buffer.pop(0)
+		try:
+			self.socket.send(data)
+		except socket.error:
+			print "UHOH"
+
+		if not self.write_buffer:
+			self.server.w_list.remove(self)
+			self.close()
+
+	def handle_request(self, request):
 		if not request:
 			self.close()
 			return
@@ -52,7 +76,6 @@ class Connection(threading.Thread):
 			'PATH_TRANSLATED': os.getcwd() + '/' + uri_headers['PATH_INFO'],
 			'SCRIPT_NAME': '',
 			'REDIRECT_STATUS': 200
-			
 		}
 
 		all_headers = dict(request_headers.items() + wsgi_environ.items() + other_headers.items() + uri_headers.items())
@@ -62,7 +85,7 @@ class Connection(threading.Thread):
 		try:
 			response.extend(self.server.app(all_headers, self.start_response))
 		except Exception, e:
-			self.status = '500 Server Error'
+			self.status = '500 Internal Server Error'
 			self.headers = [('Content-Type', 'text/html')]
 
 			e_type, e_value, e_tb = sys.exc_info()
@@ -79,8 +102,6 @@ class Connection(threading.Thread):
 			for i in response:
 				self.send(i)
 
-		self.close()
-
 	def send_headers(self, status, headers):
 		self.send("HTTP/1.1 %s\r\n" % status)
 		for k, v in headers:
@@ -93,19 +114,21 @@ class Connection(threading.Thread):
 		self.other = other
 
 	def send(self, data):
-		try:
-			self.socket.send(data)
-		except socket.error:
-			self.shutdown()
-			self.close()
+		self.write_buffer.append(data)
+
+		# Make sure connection is part of the write list
+		if self not in self.server.w_list:
+			self.server.w_list.append(self)
 
 	def shutdown(self):
 		self.socket.shutdown(socket.SHUT_RDWR)
 
 	def close(self):
 		self.socket.close()
-		if self in self.server.connections:
-			self.server.connections.remove(self)
+		if self in self.server.r_list:
+			self.server.r_list.remove(self)
+		if self in self.server.w_list:
+			self.server.w_list.remove(self)
 
 
 class HTTPServer(object):
@@ -123,7 +146,6 @@ class HTTPServer(object):
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-		self.running = True
 		if self.auto_reload:
 			self.module_monitor = modulemonitor.ModuleMonitor(self)
 
@@ -131,7 +153,14 @@ class HTTPServer(object):
 			loader=ChoiceLoader([PackageLoader('frame', 'templates'),
 				FileSystemLoader('templates')]))
 
-	def __bind_socket(self):
+		self.running = True
+
+		# Basic lists needed for select
+		self.r_list = [self.socket]
+		self.w_list = []
+		self.e_list = []
+
+	def bind_socket(self):
 		self.socket.bind((self.host, self.port))
 		self.socket.listen(self.listen)
 
@@ -145,20 +174,26 @@ class HTTPServer(object):
 		signal.signal(signal.SIGINT, self._handle_signal)
 
 	def run(self):
-		self.__bind_socket()
-
+		self.bind_socket()
 		self.setup_signal_handlers()
 
-		if self.auto_reload:
-			self.module_monitor.start()
-
 		while self.running:
-			try:
-				connection = Connection(self, self.socket.accept())
-			except socket.error:
-				continue
-			self.connections.append(connection)
-			connection.start()
+			r_ready, w_ready, e_ready = select.select(self.r_list, self.w_list, self.e_list)
+
+			for i in r_ready:
+				if i is self.socket:
+					connection = Connection(self, self.socket.accept())
+					self.r_list.append(connection)
+					connection.handle_connect()
+
+				elif isinstance(i, Connection):
+					i.handle_read()
+
+			for i in w_ready:
+				i.handle_write()
+
+			for i in e_ready:
+				i.handle_error()
 
 	def stop(self, stop_monitor=False):
 		self.running = False
